858cae51c23751a34cf2857753003f1a9094848a
lain <145809470+Ivulai@users.noreply.github.com>
2023-12-17 01:15:17 +0700

    debug not finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arrayRemoved) {
 
+
+
+    function removeCalculation(){
+        for (i = 0; i <= arrayRemoved.length; i++){
+            let index = array.indexOf(arrayRemoved[i]);
+            array.splice(index, 1);
+            console.log(i)
+            
+    }
+ 
+    }
+    let legt = arrayRemoved.length
+    return legt
+
+    
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 4))
+
+// THIS DOES NOT WORK EXPERIMENT LATER
 
 // Do not edit below this line
 module.exports = removeFromArray;
