d34773a0cb3101898f611a60681e1ff6de8408b6
lain <145809470+Ivulai@users.noreply.github.com>
2023-12-16 23:52:24 +0700

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
 
+    let stringArray = toReverse.split('')
+    let stringReverse = stringArray.reverse()
+    let stringJoinedBack = stringReverse.join('')
+
+    return stringJoinedBack
 };
 
 // Do not edit below this line
