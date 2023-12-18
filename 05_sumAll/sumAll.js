a442c9f50e90098e76983edfa4e86e38bb5570d7
lain <145809470+Lain62@users.noreply.github.com>
2023-12-19 02:21:32 +0700

    Finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function( intA, intB) {
 
+    let sum = 0
+    let minSum = Math.min(intA,intB)
+    let maxSum = Math.max(intA,intB)
+
+    if (Number.isInteger(intA) && Number.isInteger(intB)) {
+        if (intA > 0 && intB > 0) {
+            for (minSum; minSum <= maxSum; minSum += 1) {
+                sum += minSum
+            }
+        } else {
+            return "ERROR"
+        }
+    } else {
+        return "ERROR"
+    }
+
+    return sum
 };
 
 // Do not edit below this line
