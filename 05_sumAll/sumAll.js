f9154542f57eaf5b6a17b7fc49aacd68c2536297
lain <145809470+Lain62@users.noreply.github.com>
2023-12-19 02:56:09 +0700

    Refactored for never nesting

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,13 @@
 const sumAll = function( intA, intB) {
-
-    let sum = 0
-    let minSum = Math.min(intA,intB)
-    let maxSum = Math.max(intA,intB)
-
-    if (Number.isInteger(intA) && Number.isInteger(intB)) {
-        if (intA > 0 && intB > 0) {
-            for (minSum; minSum <= maxSum; minSum += 1) {
-                sum += minSum
-            }
-        } else {
-            return "ERROR"
-        }
-    } else {
-        return "ERROR"
-    }
-
-    return sum
+    if (!Number.isInteger(intA) || !Number.isInteger(intB)) return "ERROR";
+    if (intA < 0 || intB < 0) return "ERROR" ;
+    let sum = 0;
+    let minSum = Math.min(intA,intB);
+    let maxSum = Math.max(intA,intB);
+    for (minSum; minSum <= maxSum; minSum += 1) {
+        sum += minSum   ;
+    } 
+    return sum;
 };
 
 // Do not edit below this line
