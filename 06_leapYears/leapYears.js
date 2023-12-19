711a5e3dba1bb76a2086f1a92160a2330a68bd64
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-19 21:38:55 +0700

    added documentations

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,7 @@
 const leapYears = function(year) {
-
-    
-    if ( year % 100 == 0 && year % 400 != 0) return false 
-    if (year % 4 == 0) return true
-    return false
-    
+    if ( year % 100 == 0 && year % 400 != 0) return false // checks if its divisible by 100 but not divisible 400
+    if (year % 4 == 0) return true // check if divisible by 4
+    return false // all else
 };
 
 // Do not edit below this line
