db61fe6edf83df84c5b04e18507584390fa19442
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-28 00:41:04 +0700

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(...arr) {
+  let x = 0;
+  for (i=0; i < arr.length;i++){
+  for (i=0; i < arr[0].length; i++){
+    x+= arr[0][i]
+  }}
+  return x
 };
 
-const multiply = function() {
-
+const multiply = function(...arr) {
+  let x = 1;
+  for (i=0; i < arr.length;i++){
+  for (i=0; i < arr[0].length; i++){
+    x = x * arr[0][i]
+  }}
+  return x
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let z = 1
+	for (i=0; i < y; i++){
+    z = z * x
+  }
+  return z
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let z = 1
+  let y = x
+  for (i=0; i<x; i++){
+    z = z * y
+    y -= 1
+  }
+	return z
 };
 
 // Do not edit below this line
