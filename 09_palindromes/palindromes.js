d2e6d8a838c73e9a5b012d233c6b92c4a6e4ebb4
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-28 22:11:59 +0700

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (args) {
+	args = args.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase()
+	let reverse = "";
+	let i = 0;
+	while (i < args.length){
+		i++;
+		reverse += args[args.length-i]
+	}
+	if (reverse.replace(/ /g, '')== args.replace(/ /g, '')){
+		return true
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
