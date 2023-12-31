24a1c66700dc12bf11a0b49cd6365712110a5fd1
Lain <145809470+Lain62@users.noreply.github.com>
2023-12-31 09:30:06 +0700

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	let num = parseInt(n)
+	let lastnum = 0;
+	let nextnum = 1;
+	let currentnum = 1;
+	if (num == 0) return lastnum;
+	if (num < 0) return "OOPS";
+	for (i=2; i <= num; i++){
+		currentnum = lastnum + nextnum;
+		lastnum = nextnum;
+		nextnum = currentnum;
+	}
+	return currentnum;
 
 };
 
